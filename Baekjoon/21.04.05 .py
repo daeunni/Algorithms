58207bf1b5fba937c16284c73a5a994c009f1513
당니 <62705839+daeunni@users.noreply.github.com>
2021-04-05 23:28:17 +0900

    Update and rename 21.04.05 .py to Baekjoon/21.04.05 .py

--- Baekjoon/21.04.05 .py
+++ Baekjoon/21.04.05 .py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
 # coding: utf-8
-
-# In[23]:
-
 
 # 1330번 
 a, b = map(int, input().split())  # 입력 받고 공백으로 구분
@@ -12,9 +8,6 @@ elif a<b:
     print('<')
 elif a==b:
     print('==')
-
-
-# In[26]:
 
 
 # 9498번
@@ -31,9 +24,6 @@ else:
     print('F')
 
 
-# In[31]:
-
-
 # 2753번
 y = int(input())
 if y%4==0 and y%100!=0:
@@ -42,9 +32,6 @@ elif y%400==0:
     print(1)
 else:
     print(0)        
-
-
-# In[64]:
 
 
 # 14681번 : 사분면 고르기
@@ -59,9 +46,6 @@ else:
     print(4)
 
 
-# In[38]:
-
-
 # 14681번 : 사분면 고르기
 a = int(input())
 b = int(input())
@@ -73,9 +57,6 @@ elif a<0 and b>0:
     print(2)
 elif a<0 and b<0:
     print(3)
-
-
-# In[54]:
 
 
 # 2884번 : 알람 시계
@@ -98,16 +79,11 @@ elif h==0:
         print(hour//60+12, hour%60)  # 고려해야할 반례 : 0시 45분
 
 
-# In[55]:
-
-
 # 2884번 더 쉽게....
 a, b= map(int, input().split())
 x = a*60+b-45
 print(x//60%24, x%60)   # 하아....난 멀었다 멀었어....
 
-
-# In[75]:
 
 
 # 10871번 : x보다 작은 수
@@ -119,8 +95,6 @@ for i in list(a):
         b.append(str(i))
 print(" ".join(b), end="")  # join 함수 사용!
 
-
-# In[77]:
 
 
 # 10952번 : A+B-5    
